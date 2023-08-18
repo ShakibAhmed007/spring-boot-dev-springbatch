d746a7d7867967a8ff8a635f14003d4c816c4977
unknown <shakibahmed09112017@gmail.com>
2023-08-18 11:52:30 +0600

    first commit

---
+++ src/main/java/com/example/springbatch/SpringBootDevSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevSpringbatchApplication.class, args);
+	}
+
+}
