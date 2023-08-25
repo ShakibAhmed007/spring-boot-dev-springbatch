ec29c35995de163040a5ee6b04a74a5cfe8b578b
unknown <shakibahmed09112017@gmail.com>
2023-08-25 13:13:30 +0600

    update

--- src/main/java/com/example/springbatch/SpringBootDevSpringbatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBootDevSpringbatchApplication.java
@@ -1,9 +1,13 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableBatchProcessing
 public class SpringBootDevSpringbatchApplication {
 
 	public static void main(String[] args) {
